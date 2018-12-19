47b7e987a6b4e57cfcdf692d40a8958a75800bb8
zhaohongbin <zhaohongbin@le.com>
2018-12-20 01:55:17 +0800

    修改功能键界面

--- pages/control/function/function.js
+++ pages/control/function/function.js
@@ -1,4 +1,5 @@
 // pages/control/function/function.js
+const app = getApp();
 Page({
 
   /**
@@ -12,7 +13,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var car = JSON.parse(options.car);
+    // var car = JSON.parse(options.car);
+    var cars = wx.getStorageSync('cars')
+    var car = cars[0]
     console.log(car)
     var functionKeysArr = [];
     var functionArr = [];
